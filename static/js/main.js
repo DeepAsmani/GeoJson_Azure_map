fc34fb2a84556f49d5e17b2f20b4f74a89b8af6f
DeepAsmani <asmanideeped@gmail.com>
2023-11-01 18:03:08 +0530

    01-11-2023-18:02

--- static/js/main.js
+++ static/js/main.js
@@ -11,177 +11,178 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   map.events.add("ready", function () {
-    map.imageSprite
-      .add("gym", "static/img/gym.png")
-      .then(function () {
-        map.controls.add(
-          new atlas.control.StyleControl({
-            mapStyles: "all",
-          }),
-          {
-            position: "top-right",
-          }
-        );
-        var wardDataSource = new atlas.source.DataSource();
-        map.sources.add(wardDataSource);
+    map.controls.add(
+      new atlas.control.StyleControl({
+        mapStyles: "all",
+      }),
+      {
+        position: "top-right",
+      }
+    );
+    var wardDataSource = new atlas.source.DataSource();
+    map.sources.add(wardDataSource);
 
-        map.layers.add(
-          new atlas.layer.PolygonLayer(wardDataSource, null, {
-            fillColor: "gray",
-            fillOpacity: 0.7,
-          })
-        );
+    map.layers.add(
+      new atlas.layer.PolygonLayer(wardDataSource, null, {
+        fillColor: "gray",
+        fillOpacity: 0.7,
+      })
+    );
 
-        wardDataSource.importDataFromUrl(wardGeojson);
+    wardDataSource.importDataFromUrl(wardGeojson);
 
-        var librariesDataSource = new atlas.source.DataSource();
-        map.sources.add(librariesDataSource);
+    map.imageSprite.add("library", "static/img/Library.png").then(function () {
+      var librariesDataSource = new atlas.source.DataSource();
+      map.sources.add(librariesDataSource);
 
-        map.layers.add(
-          new atlas.layer.SymbolLayer(librariesDataSource, null, {
-            iconOptions: {
-              image: "pin-round-red",
-              allowOverlap: true,
-              ignorePlacement: true,
-            },
-            filter: [
-              "any",
-              ["==", ["geometry-type"], "Point"],
-              ["==", ["geometry-type"], "MultiPoint"],
-            ],
-          })
-        );
+      map.layers.add(
+        new atlas.layer.SymbolLayer(librariesDataSource, null, {
+          iconOptions: {
+            image: "library",
+            size: 0.2,
+            allowOverlap: true,
+            ignorePlacement: true,
+          },
+          filter: [
+            "any",
+            ["==", ["geometry-type"], "Point"],
+            ["==", ["geometry-type"], "MultiPoint"],
+          ],
+        })
+      );
+      librariesDataSource.importDataFromUrl(librariesGeojson);
+    });
 
-        librariesDataSource.importDataFromUrl(librariesGeojson);
+    map.imageSprite.add("gym", "static/img/gym.png").then(function () {
+      var gymsDataSource = new atlas.source.DataSource();
+      map.sources.add(gymsDataSource);
+      map.layers.add(
+        new atlas.layer.SymbolLayer(gymsDataSource, null, {
+          iconOptions: {
+            image: "gym",
+            allowOverlap: true,
+            ignorePlacement: true,
+            size: 0.1,
+          },
+          filter: [
+            "any",
+            ["==", ["geometry-type"], "Point"],
+            ["==", ["geometry-type"], "MultiPoint"],
+          ],
+        })
+      );
+      gymsDataSource.importDataFromUrl(gymsGeojson);
+    });
 
-        var gymsDataSource = new atlas.source.DataSource();
-        map.sources.add(gymsDataSource);
-        map.layers.add(
-          new atlas.layer.SymbolLayer(gymsDataSource, null, {
-            iconOptions: {
-              image: "gym",
-              allowOverlap: true,
-              ignorePlacement: true,
-              size:0.1
-            },
-            filter: [
-              "any",
-              ["==", ["geometry-type"], "Point"],
-              ["==", ["geometry-type"], "MultiPoint"],
-            ],
-          })
-        );
-        gymsDataSource.importDataFromUrl(gymsGeojson);
+    var swimmingPoolsDataSource = new atlas.source.DataSource();
+    map.sources.add(swimmingPoolsDataSource);
+    map.layers.add(
+      new atlas.layer.SymbolLayer(swimmingPoolsDataSource, null, {
+        iconOptions: {
+          image: "marker-darkblue",
+          allowOverlap: true,
+          ignorePlacement: true,
+        },
+        filter: [
+          "any",
+          ["==", ["geometry-type"], "Point"],
+          ["==", ["geometry-type"], "MultiPoint"],
+        ],
+      })
+    );
+    swimmingPoolsDataSource.importDataFromUrl(swimmingPoolsGeojson);
 
-        var swimmingPoolsDataSource = new atlas.source.DataSource();
-        map.sources.add(swimmingPoolsDataSource);
-        map.layers.add(
-          new atlas.layer.SymbolLayer(swimmingPoolsDataSource, null, {
-            iconOptions: {
-              image: "marker-darkblue",
-              allowOverlap: true,
-              ignorePlacement: true,
-            },
-            filter: [
-              "any",
-              ["==", ["geometry-type"], "Point"],
-              ["==", ["geometry-type"], "MultiPoint"],
-            ],
-          })
-        );
-        swimmingPoolsDataSource.importDataFromUrl(swimmingPoolsGeojson);
+    var zonalOfficeDataSource = new atlas.source.DataSource();
+    map.sources.add(zonalOfficeDataSource);
+    map.layers.add(
+      new atlas.layer.SymbolLayer(zonalOfficeDataSource, null, {
+        iconOptions: {
+          image: "marker-blue",
+          allowOverlap: true,
+          ignorePlacement: true,
+        },
+        filter: [
+          "any",
+          ["==", ["geometry-type"], "Point"],
+          ["==", ["geometry-type"], "MultiPoint"],
+        ],
+      })
+    );
+    zonalOfficeDataSource.importDataFromUrl(zonalOfficeGeojson);
 
-        var zonalOfficeDataSource = new atlas.source.DataSource();
-        map.sources.add(zonalOfficeDataSource);
-        map.layers.add(
-          new atlas.layer.SymbolLayer(zonalOfficeDataSource, null, {
-            iconOptions: {
-              image: "marker-blue",
-              allowOverlap: true,
-              ignorePlacement: true,
-            },
-            filter: [
-              "any",
-              ["==", ["geometry-type"], "Point"],
-              ["==", ["geometry-type"], "MultiPoint"],
-            ],
-          })
-        );
-        zonalOfficeDataSource.importDataFromUrl(zonalOfficeGeojson);
+    var wardOfficeDataSource = new atlas.source.DataSource();
+    map.sources.add(wardOfficeDataSource);
+    map.layers.add(
+      new atlas.layer.SymbolLayer(wardOfficeDataSource, null, {
+        iconOptions: {
+          image: "marker-yellow",
+          allowOverlap: true,
+          ignorePlacement: true,
+        },
+        filter: [
+          "any",
+          ["==", ["geometry-type"], "Point"],
+          ["==", ["geometry-type"], "MultiPoint"],
+        ],
+      })
+    );
 
-        var wardOfficeDataSource = new atlas.source.DataSource();
-        map.sources.add(wardOfficeDataSource);
-        map.layers.add(
-          new atlas.layer.SymbolLayer(wardOfficeDataSource, null, {
-            iconOptions: {
-              image: "marker-yellow",
-              allowOverlap: true,
-              ignorePlacement: true,
-            },
-            filter: [
-              "any",
-              ["==", ["geometry-type"], "Point"],
-              ["==", ["geometry-type"], "MultiPoint"],
-            ],
-          })
-        );
-        wardOfficeDataSource.importDataFromUrl(wardOfficeGeojson);
+    wardOfficeDataSource.importDataFromUrl(wardOfficeGeojson);
 
-        legend = new atlas.control.LegendControl({
-          title: "Ahemdabad City's Municipal Corporation Facilty",
-          showToggle: true,
-          legends: [
+    legend = new atlas.control.LegendControl({
+      title: "Ahemdabad City's Municipal Corporation Facilty",
+      showToggle: true,
+      legends: [
+        {
+          type: "category",
+          subtitle: "Facilty",
+          layout: "column",
+          itemLayout: "row",
+          footer: "",
+          strokeWidth: 3,
+          items: [
             {
-              type: "category",
-              subtitle: "Facilty",
-              layout: "column",
-              itemLayout: "row",
-              footer: "",
-              strokeWidth: 3,
-              items: [
-                {
-                  color: "Gray",
-                  label: "Wards",
-                  shape: "circle",
-                },
-                {
-                  color: "Red",
-                  label: "Libraries",
-                  image: "circle",
-                },
-                {
-                  shape: 'static/img/gym.png',
-                  size: 0.1,
-                  color: "Black",
-                  label: "Gyms",
-                  shape: "circle",
-                  alt: 'Campfire'
-                },
-                {
-                  color: "Darkblue",
-                  label: "Swimming Pools",
-                  shape: "circle",
-                },
-                {
-                  color: "Blue",
-                  label: "Zone Offices",
-                  shape: "circle",
-                },
-                {
-                  color: "Yellow",
-                  label: "Ward Offices",
-                  shape: "circle",
-                },
-              ],
+              color: "Gray",
+              label: "Wards",
+              shape: "circle",
+            },
+            {
+              color: "Red",
+              size: 0.1,
+              label: "Libraries",
+              shape: "static/img/Library.png",
+            },
+            {
+              shape: "/static/img/gym.png",
+              size: 0.1,
+              color: "Black",
+              label: "Gyms",
+            },
+            {
+              color: "Darkblue",
+              label: "Swimming Pools",
+              shape: "circle",
+            },
+            {
+              color: "Blue",
+              label: "Zone Offices",
+              shape: "circle",
+            },
+            {
+              color: "Yellow",
+              label: "Ward Offices",
+              shape: "circle",
             },
           ],
-        });
-        map.controls.add(legend, {
-          position: "bottom-left",
-        });
-      });
+        },
+      ],
+    });
+    map.controls.add(legend, {
+      position: "bottom-left",
+    });
   });
 });
+
 function zoomMap(offset) {
   var cam = map.getCamera();
 
@@ -217,4 +218,43 @@ function rotateMap(offset) {
     type: "ease",
     duration: 250,
   });
+}
+
+function featureClicked(e) {
+  //Make sure the event occurred on a shape feature.
+  if (e.shapes && e.shapes.length > 0) {
+      //By default, show the popup where the mouse event occurred.
+      var pos = e.position;
+      var offset = [0, 0];
+      var properties;
+
+      if (e.shapes[0] instanceof atlas.Shape) {
+          properties = e.shapes[0].getProperties();
+
+          //If the shape is a point feature, show the popup at the points coordinate.
+          if (e.shapes[0].getType() === 'Point') {
+              pos = e.shapes[0].getCoordinates();
+              offset = [0, -18];
+          }
+      } else {
+          properties = e.shapes[0].properties;
+
+          //If the shape is a point feature, show the popup at the points coordinate.
+          if (e.shapes[0].type === 'Point') {
+              pos = e.shapes[0].geometry.coordinates;
+              offset = [0, -18];
+          }
+      }
+      console.log(e.shapes[0])
+      //Update the content and position of the popup.
+      popup.setOptions({
+          //Create a table from the properties in the feature.
+          content: atlas.PopupTemplate.applyTemplate(properties),
+          position: pos,
+          pixelOffset: offset
+      });
+
+      //Open the popup.
+      popup.open(map);
+  }
 }
