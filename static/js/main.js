4720bf0b8ffe2e5150b3b13c3d83f7f09e085783
DeepAsmani <asmanideeped@gmail.com>
2023-11-02 14:21:08 +0530

    02-11-2023-14:21

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,6 @@
 var map;
 var popup;
+
 document.addEventListener("DOMContentLoaded", function () {
   map = new atlas.Map("map", {
     center: [cityLongitude, cityLatitude],
@@ -29,133 +30,63 @@ document.addEventListener("DOMContentLoaded", function () {
     map.sources.add(wardDataSource);
     var ward = new atlas.layer.PolygonLayer(wardDataSource, null, {
       fillColor: "gray",
-      fillOpacity: 0.7,
+      fillOpacity: 0.3,
     });
     map.events.add("click", ward, featureClicked);
     map.layers.add(ward);
     wardDataSource.importDataFromUrl(wardGeojson);
 
-    map.imageSprite.add("library", "static/img/Library.png").then(function () {
-      var librariesDataSource = new atlas.source.DataSource();
-      map.sources.add(librariesDataSource);
-      var library = new atlas.layer.SymbolLayer(librariesDataSource, null, {
-        iconOptions: {
-          image: "library",
-          size: 0.12,
-          allowOverlap: true,
-          ignorePlacement: true,
-        },
-        filter: [
-          "any",
-          ["==", ["geometry-type"], "Point"],
-          ["==", ["geometry-type"], "MultiPoint"],
-        ],
-      });
-      map.events.add("click", library, featureClicked);
-      map.layers.add(library);
-      librariesDataSource.importDataFromUrl(librariesGeojson);
-    });
 
-    map.imageSprite.add("gym", "static/img/gym.png").then(function () {
-      var gymsDataSource = new atlas.source.DataSource();
-      map.sources.add(gymsDataSource);
-      var gym = new atlas.layer.SymbolLayer(gymsDataSource, null, {
-        iconOptions: {
-          image: "gym",
-          allowOverlap: true,
-          ignorePlacement: true,
-          size: 0.1,
-        },
-        filter: [
-          "any",
-          ["==", ["geometry-type"], "Point"],
-          ["==", ["geometry-type"], "MultiPoint"],
-        ],
-      });
-      map.events.add("click", gym, featureClicked);
-      map.layers.add(gym);
-      gymsDataSource.importDataFromUrl(gymsGeojson);
-    });
+    function createSymbolLayer(dataSource, iconName, geojsonUrl, iconSize) {
+      map.imageSprite
+        .add(iconName, "static/img/" + iconName + ".png")
+        .then(function () {
+          var symbolLayer = new atlas.layer.SymbolLayer(dataSource, null, {
+            iconOptions: {
+              image: iconName,
+              size: iconSize,
+              allowOverlap: true,
+              ignorePlacement: true,
+            },
+            filter: [
+              "any",
+              ["==", ["geometry-type"], "Point"],
+              ["==", ["geometry-type"], "MultiPoint"],
+            ],
+          });
+          map.events.add("click", symbolLayer, featureClicked);
+          map.layers.add(symbolLayer);
+          dataSource.importDataFromUrl(geojsonUrl);
+        });
+    }
 
-    var swimmingPoolsDataSource = new atlas.source.DataSource();
-    map.sources.add(swimmingPoolsDataSource);
-    var swimmingPools = new atlas.layer.SymbolLayer(
-      swimmingPoolsDataSource,
-      null,
-      {
-        iconOptions: {
-          image: "marker-darkblue",
-          allowOverlap: true,
-          ignorePlacement: true,
-        },
-        filter: [
-          "any",
-          ["==", ["geometry-type"], "Point"],
-          ["==", ["geometry-type"], "MultiPoint"],
-        ],
-      }
+    // Function to create Symbol Layer and DataSource
+    function createSymbolLayerAndDataSource(iconName, geojsonUrl, iconSize) {
+      var dataSource = new atlas.source.DataSource();
+      map.sources.add(dataSource);
+      createSymbolLayer(dataSource, iconName, geojsonUrl, iconSize);
+    }
+
+    createSymbolLayerAndDataSource("library", librariesGeojson, 0.12);
+    createSymbolLayerAndDataSource("gym", gymsGeojson, 0.1);
+    createSymbolLayerAndDataSource(
+      "swimmingPools",
+      swimmingPoolsGeojson,
+      0.1
     );
-    map.events.add("click", swimmingPools, featureClicked);
-    map.layers.add(swimmingPools);
-    swimmingPoolsDataSource.importDataFromUrl(swimmingPoolsGeojson);
-
-    map.imageSprite.add("zonal", "static/img/offices.png").then(function () {
-      var zonalOfficeDataSource = new atlas.source.DataSource();
-      map.sources.add(zonalOfficeDataSource);
-      var zoneoffice = new atlas.layer.SymbolLayer(
-        zonalOfficeDataSource,
-        null,
-        {
-          iconOptions: {
-            image: "zonal",
-            allowOverlap: true,
-            ignorePlacement: true,
-            size: 0.1,
-          },
-          filter: [
-            "any",
-            ["==", ["geometry-type"], "Point"],
-            ["==", ["geometry-type"], "MultiPoint"],
-          ],
-        }
-      );
-      map.events.add("click", zoneoffice, featureClicked);
-      map.layers.add(zoneoffice);
-      zonalOfficeDataSource.importDataFromUrl(zonalOfficeGeojson);
-    });
-
-    // map.imageSprite.add("wardoffice", "static/img/office.png").then(function () {
-    //   var wardOfficeDataSource = new atlas.source.DataSource();
-    //   map.sources.add(wardOfficeDataSource);
-    //   var wardOffice = new atlas.layer.SymbolLayer(wardOfficeDataSource, null, {
-    //     iconOptions: {
-    //       image: "wardoffice",
-    //       allowOverlap: true,
-    //       ignorePlacement: true,
-    //       size:0.1
-    //     },
-    //     filter: [
-    //       "any",
-    //       ["==", ["geometry-type"], "Point"],
-    //       ["==", ["geometry-type"], "MultiPoint"],
-    //     ],
-    //   });
-    //   map.events.add("click", wardOffice, featureClicked);
-    //   map.layers.add(wardOffice);
-    //   wardOfficeDataSource.importDataFromUrl(wardOfficeGeojson);
-    // });
+    createSymbolLayerAndDataSource("offices", zonalOfficeGeojson, 0.1);
 
     legend = new atlas.control.LegendControl({
-      title: "Ahemdabad City's Municipal Corporation Facilty",
+      title: "Ahemdabad City's Municipal Corporation Facility",
       showToggle: true,
       legends: [
         {
           type: "category",
-          subtitle: "Facilty",
+          subtitle: "Facility",
           layout: "column",
           itemLayout: "row",
           footer: "",
-          strokeWidth: 3,
+          strokeWidth: 1,
           items: [
             {
               color: "Gray",
@@ -164,113 +95,100 @@ document.addEventListener("DOMContentLoaded", function () {
             },
             {
               color: "Red",
-              size: 0.1,
+              size: 1,
               label: "Libraries",
               shape: "static/img/Library.png",
             },
             {
               shape: "/static/img/gym.png",
-              size: 0.1,
+              size: 1,
               color: "Black",
               label: "Gyms",
             },
             {
               color: "Darkblue",
               label: "Swimming Pools",
-              shape: "circle",
+              shape: "/static/img/swimmingPools.png",
             },
             {
               color: "Blue",
               label: "Zone Offices",
               shape: "static/img/offices.png",
             },
-            // {
-            //   color: "Yellow",
-            //   label: "Ward Offices",
-            //   shape: "static/img/office.png",
-            // },
           ],
         },
       ],
     });
+
     map.controls.add(legend, {
       position: "bottom-left",
     });
   });
-});
 
-function zoomMap(offset) {
-  var cam = map.getCamera();
+  // Function to handle map zoom
+  function zoomMap(offset) {
+    var cam = map.getCamera();
 
-  map.setCamera({
-    //Zoom the map within the range of min/max zoom of the map.
-    zoom: Math.max(cam.minZoom, Math.min(cam.maxZoom, cam.zoom + offset)),
-    type: "ease",
-    duration: 250,
-  });
-}
-
-// Number of degrees to change pitch the map per click.
-const pitchStep = 10;
-
-function pitchMap(offset) {
-  map.setCamera({
-    // Pitch the map within the range of 0 - 60 degrees.
-    pitch: Math.max(
-      0,
-      Math.min(60, map.getCamera().pitch + offset * pitchStep)
-    ),
-    type: "ease",
-    duration: 250,
-  });
-}
-
-// Number of degrees to change rotate the map per click.
-const bearingStep = 15;
-
-function rotateMap(offset) {
-  map.setCamera({
-    bearing: map.getCamera().bearing + offset * bearingStep,
-    type: "ease",
-    duration: 250,
-  });
-}
-
-function featureClicked(e) {
-  //Make sure the event occurred on a shape feature.
-  if (e.shapes && e.shapes.length > 0) {
-    //By default, show the popup where the mouse event occurred.
-    var pos = e.position;
-    var offset = [0, 0];
-    var properties;
-
-    if (e.shapes[0] instanceof atlas.Shape) {
-      properties = e.shapes[0].getProperties();
-
-      //If the shape is a point feature, show the popup at the points coordinate.
-      if (e.shapes[0].getType() === "Point") {
-        pos = e.shapes[0].getCoordinates();
-        offset = [0, -18];
-      }
-    } else {
-      properties = e.shapes[0].properties;
-
-      //If the shape is a point feature, show the popup at the points coordinate.
-      if (e.shapes[0].type === "Point") {
-        pos = e.shapes[0].geometry.coordinates;
-        offset = [0, -18];
-      }
-    }
-    console.log(e.shapes[0]);
-    //Update the content and position of the popup.
-    popup.setOptions({
-      //Create a table from the properties in the feature.
-      content: atlas.PopupTemplate.applyTemplate(properties),
-      position: pos,
-      pixelOffset: offset,
+    map.setCamera({
+      zoom: Math.max(cam.minZoom, Math.min(cam.maxZoom, cam.zoom + offset)),
+      type: "ease",
+      duration: 250,
     });
-
-    //Open the popup.
-    popup.open(map);
   }
-}
+
+  // Function to handle map pitch
+  const pitchStep = 10;
+
+  function pitchMap(offset) {
+    map.setCamera({
+      pitch: Math.max(
+        0,
+        Math.min(60, map.getCamera().pitch + offset * pitchStep)
+      ),
+      type: "ease",
+      duration: 250,
+    });
+  }
+
+  // Function to handle map rotation
+  const bearingStep = 15;
+
+  function rotateMap(offset) {
+    map.setCamera({
+      bearing: map.getCamera().bearing + offset * bearingStep,
+      type: "ease",
+      duration: 250,
+    });
+  }
+
+  // Function to handle feature click event
+  function featureClicked(e) {
+    if (e.shapes && e.shapes.length > 0) {
+      var pos = e.position;
+      var offset = [0, 0];
+      var properties;
+
+      if (e.shapes[0] instanceof atlas.Shape) {
+        properties = e.shapes[0].getProperties();
+        if (e.shapes[0].getType() === "Point") {
+          pos = e.shapes[0].getCoordinates();
+          offset = [0, -18];
+        }
+      } else {
+        properties = e.shapes[0].properties;
+        if (e.shapes[0].type === "Point") {
+          pos = e.shapes[0].geometry.coordinates;
+          offset = [0, -18];
+        }
+      }
+
+      popup.setOptions({
+        content: atlas.PopupTemplate.applyTemplate(properties),
+        position: pos,
+        pixelOffset: offset,
+      });
+
+      popup.open(map);
+    }
+  }
+});
